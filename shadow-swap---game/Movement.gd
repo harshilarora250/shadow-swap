ce6c4050e7ffdafea5473b7fc4d29972aece132e
Web Creater <84850386+harshilarora250@users.noreply.github.com>
2026-07-24 02:53:49 +0400

    Movement completed!

---
+++ shadow-swap---game/Movement.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+@onready var sprite = $Sprite2D
+
+func _on_move_right_pressed() -> void:
+	sprite.position.x +=20
+
+
+func _on_move_left_pressed() -> void:
+	sprite.position.x -= 20
+
+
+func _on_jump_pressed() -> void:
+	sprite.position.y -= 100
+	await get_tree().create_timer(1.2).timeout
+	sprite.position.y += 100
