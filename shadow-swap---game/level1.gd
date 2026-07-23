2b040f70570b08f409182241c5904fb6250b0f9c
Web Creater <84850386+harshilarora250@users.noreply.github.com>
2026-07-24 02:08:19 +0400

    Main Menu - Level 1 btn to level 1

---
+++ shadow-swap---game/level1.gd
@@ -0,0 +1,11 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Level1.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
