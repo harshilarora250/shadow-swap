514935076f2b56dcfb414b243700ee4ea8e077f5
Web Creater <84850386+harshilarora250@users.noreply.github.com>
2026-07-24 05:50:33 +0400

    Kill system don't work

--- shadow-swap---game/Movement.gd
+++ shadow-swap---game/Movement.gd
@@ -22,5 +22,9 @@ func _on_move_left_pressed() -> void:
 
 func _on_jump_pressed() -> void:
 	sprite.position.y -= 100
-	await get_tree().create_timer(1.2).timeout
+	await get_tree().create_timer(1.4).timeout
 	sprite.position.y += 100
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("Hit!")
+	get_tree().reload_current_scene()
